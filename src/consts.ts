22a3347839f3919b72abb360efbfbf39c90816b2
aabuhijleh <aabuhijleh.dev@gmail.com>
2026-09-17 16:48:03 +0300

    refactor: reuse the author name constant in the footer

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
 export const SITE_TITLE = "Abed's Blog";
 export const SITE_DESCRIPTION = "A technical blog about building for the web.";
 export const SITE_AUTHOR = "Abed";
+export const SITE_AUTHOR_FULL_NAME = "Abed Abu-Hijleh";
