52e109a51c3457f43694d53d83c1da01fcf84df2
aabuhijleh <aabuhijleh.dev@gmail.com>
2026-09-18 12:23:33 +0300

    feat: rewrite the bio and share the social links

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,12 @@ export const SITE_AUTHOR = "Abed";
 export const SITE_AUTHOR_FULL_NAME = "Abed Abu-Hijleh";
 
 export const CODE_THEME = "nord";
+
+export const SOCIAL_LINKS = {
+  website: "https://aabuhijleh.com",
+  github: "https://github.com/aabuhijleh",
+  linkedin: "https://www.linkedin.com/in/aabuhijleh/",
+  x: "https://x.com/aabuhijleh_dev",
+  stackOverflow: "https://stackoverflow.com/users/9698583/aabuhijleh",
+  email: "mailto:aabuhijleh.dev@gmail.com",
+} as const;
